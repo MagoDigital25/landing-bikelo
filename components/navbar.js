5053c2a8756ada58a625a0c633607ec1d19bc861
Mago <MagoDigi@users.noreply.huggingface.co>
2025-10-28 17:21:28 +0000

    Manual changes saved

--- components/navbar.js
+++ components/navbar.js
@@ -102,7 +102,7 @@ class CustomNavbar extends HTMLElement {
           <i data-feather="menu"></i>
         </button>
         <ul>
-          <li><a href="#features" class="active">Solución</a></li>
+          <li><a href="#portada" class="active">Solución</a></li>
           <li hidden><a href="#pricing">Precios</a></li>
           <li><a href="#funciones">¿Que hace?</a></li>
           <li><a href="#beneficios">Beneficios</a></li>
